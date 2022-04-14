861fafc39a247b2d54506d8c3003ba3a4139713f
William Caine <william.caineiii@gmail.com>
2022-04-14 12:22:22 -0400

    renaming library, getting to compile

--- rollup.config.js
+++ rollup.config.js
@@ -9,6 +9,7 @@ const PROD = !!process.env.CI
 export default {
   input: 'src/index.ts',
   context: 'globalThis',
+  external: [/@dcl\//, /@decentraland\//],
   output: [
     {
       file: packageJson.main,

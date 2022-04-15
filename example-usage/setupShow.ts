4b9f12cc9edf7e50ccd5d7f46e19170a4598991f
William Caine <william.caineiii@gmail.com>
2022-04-15 11:49:05 -0400

    update example to match refactor

--- example-usage/setupShow.ts
+++ example-usage/setupShow.ts
@@ -8,7 +8,7 @@ import { RunOfShow } from '../showMgmt/runOfShowSystem'
 import { PlayShowEvent, StopShowEvent } from "../showMgmt/types"
 import { ShowEntityModel } from '../syncable'
 import {  ShowActionManager,  } from "../showMgmt/manageShowActions"
-import {  ShowAnimationActionHandler, ShowAnounceActionHandler, ShowStringActionHandler,ActionHandlerAnimationParams, ActionHandlerAnouncementParams,  } from "../showMgmt/showActionHandlers"
+import {  ShowAnimationActionHandler, ShowAnounceActionHandler, ActionHandlerAnimationParams, ActionHandlerAnouncementParams, ShowStopAllActionHandler,  ShowActionHandlerSupport, ShowBasicActionHandler, ActionParams, parseActionWithOpts, ShowPauseAllActionHandler,  } from "../showMgmt/showActionHandlers"
 import { videoMat } from "../videoScreens"
 import { hideArtistName, hideBoard, setArtistName } from "./nextShowCounter"
   
@@ -56,60 +56,100 @@ SHOW_MGR.addVideoStatusChangeListener( new VideoChangeStatusListener((oldStatus:
 } ))
 
 SHOW_MGR.actionMgr.registeredHandler(
-  new  ShowStringActionHandler( {
-    name:"SAY_HI", 
-    matches: (action: string, showActionMgr: ShowActionManager): boolean => {
-      return action == "SAY_HI"
-    },
-    process(action: string, showActionMgr: ShowActionManager): boolean {
-      ui.displayAnnouncement('HI',1)
+  new  ShowBasicActionHandler( 
+    "SAY_HI",
+    {
+      process(action: ActionParams<string>, showActionMgr: ShowActionManager): boolean {
+        ui.displayAnnouncement('HI',1)
+        return true 
+      }
+    } )
+)
 
+
+SHOW_MGR.actionMgr.registeredHandler(
+  new  ShowActionHandlerSupport<string>( 
+    "SAY",
+    {
+      matches(action: string,showActionMgr:ShowActionManager):boolean{ 
+        return action.indexOf(this.name + " ") == 0
+      },
+      decodeAction(action: string, showActionMgr: ShowActionManager):ActionParams<string>{
+        log("decodeAction called for ",action)  
+        const decoded = parseActionWithOpts<string>(action)
+        decoded.params = decoded.array[1]
+        return  decoded;
+      },
+      process(action: ActionParams<string>, showActionMgr: ShowActionManager): boolean {
+        ui.displayAnnouncement(action.params,1)
+
+        return true
+      }
+    } )
+)
+
+SHOW_MGR.actionMgr.registeredHandler(
+  new ShowAnounceActionHandler( {
+    process(action: ActionParams<ActionHandlerAnouncementParams>, showActionMgr: ShowActionManager): void {
+      ui.displayAnnouncement(action.params.text,action.params.duration)
+    }
+  } )
+)
+
+SHOW_MGR.actionMgr.registeredHandler(
+  new ShowStopAllActionHandler( {
+    process(action: ActionParams<string>, showActionMgr: ShowActionManager): boolean {
+      //stop action goes here
+      //some actions "stop" is a play or hide or show or stop
       return true
     }
   } )
-)
+) 
 
-const ANNOUNCE_ACTION_NAME = "ANNOUNCE"
 SHOW_MGR.actionMgr.registeredHandler(
-  new ShowAnounceActionHandler( {
-    name:ANNOUNCE_ACTION_NAME,
-    process(action: ActionHandlerAnouncementParams, showActionMgr: ShowActionManager): void {
-      ui.displayAnnouncement(action.text,action.duration)
+  new ShowPauseAllActionHandler( {
+    process(action: ActionParams<string>, showActionMgr: ShowActionManager): boolean {
+      //pause action goes here
+      //some actions "stop" is a play or hide or show or stop
+      return true
     }
   } )
-)
+) 
+
 
 SHOW_MGR.actionMgr.registeredHandler(
-  new  ShowAnimationActionHandler( {
-    name: ShowAnimationActionHandler.DEFAULT_NAME,
-    process(action: ActionHandlerAnimationParams, showActionMgr: ShowActionManager): void {
-      const target = showActionMgr.getShowEntityByName(action.target)
-       
-      if(!target){
-        log("WARNING could not find target",action)
-        return
-      } 
-      log("animate",action)
-      const showEntity = target as ShowEntityModel
+  new  ShowAnimationActionHandler( 
+    {
+      //name: ShowAnimationActionHandler.DEFAULT_NAME,
+      process(actionResult: ActionParams<ActionHandlerAnimationParams>, showActionMgr: ShowActionManager): void {
+        const action = actionResult.params
+        const target = showActionMgr.getShowEntityByName(action.target)
+        
+        if(!target){
+          log("WARNING could not find target",action)
+          return
+        } 
+        log("animate",action)
+        const showEntity = target as ShowEntityModel
 
-      const noLoop = (action.loop !== undefined && action.loop==false)
+        const noLoop = (action.loop !== undefined && action.loop==false)
 
-      if(action.play === undefined || action.play){
-        showEntity.playAnimation(  
-          action.animationName,
-          noLoop, 
-          action.duration,
-          action.speed,
-          action.interval,
-          (action.resetAnimation === undefined || action.resetAnimation),
-          action.layer 
-        )
-      }else{
-        log("calling stop!!")
-        showEntity.stop()
+        if(action.play === undefined || action.play){
+          showEntity.playAnimation(  
+            action.animationName,
+            noLoop, 
+            action.duration,
+            action.speed,
+            action.interval,
+            (action.resetAnimation === undefined || action.resetAnimation),
+            action.layer 
+          )
+        }else{
+          log("calling stop!!")
+          showEntity.stop()
+        }
       }
-    }
-  } )
+    } )
 )
 
 

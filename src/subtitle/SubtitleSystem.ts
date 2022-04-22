38173a3b8808128855c2bb9068898f2f68ffae50
William Caine <william.caineiii@gmail.com>
2022-04-22 09:49:15 -0400

    adding comment, removing commented out code

--- src/subtitle/SubtitleSystem.ts
+++ src/subtitle/SubtitleSystem.ts
@@ -181,10 +181,7 @@ export class SubtitleSystem implements ISystem {
   update(dt: number) {
     if (this.offsetMs != SubtitleSystem.INVALID_OFFSET && !this.paused) {
       let newOffset = this.offsetMs + dt * 1000.0
-      //take it back.  and why like this, does not seem to do it right
-      /*while (newOffset > this.maxEndOffsetMs) {
-        newOffset -= this.maxEndOffsetMs
-      }*/
+      //if past end, pin it to end
       if(this.maxEndOffsetMs > 0 && newOffset > this.maxEndOffsetMs){
         newOffset = this.maxEndOffsetMs
         this.paused = true //at the end, pause/stop it

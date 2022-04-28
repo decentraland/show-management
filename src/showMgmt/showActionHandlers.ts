890acaaa0ba3ab1cd93b1f3968e045a6b143f4d4
William Caine <william.caineiii@gmail.com>
2022-04-28 11:12:21 -0400

    WIP for DefineActionGroupActionHandler

--- src/showMgmt/showActionHandlers.ts
+++ src/showMgmt/showActionHandlers.ts
@@ -228,6 +228,10 @@ export type DefineTargetGroupType={
   targets?:any[]
   targetNames?:string[]
 }
+export type DefineActionGroupType={
+  actions?:string[]
+}
+
 export type DefineActionParams<T>={
   type:string
   name:string
@@ -334,6 +338,46 @@ export class DefineActionAliasActionHandler extends ShowActionHandlerSupport<Def
     return true
   }
 }
+
+export class DefineActionGroupActionHandler extends ShowActionHandlerSupport<DefineActionParams<DefineActionGroupType>>{
+  public static DEFAULT_NAME = 'DEFINE ACTION_GROUP'
+  constructor(args?:ShowActionSupportArgs<DefineActionParams<DefineActionGroupType>>){
+    super(DefineActionAliasActionHandler.DEFAULT_NAME,args)
+  }
+
+  matches(action: string, showActionMgr: ShowActionManager): boolean {
+    //log('CUSTOM matches SAY fired',action)
+    return actionStartsWith(action,this.name,0," ")
+  }
+  decodeAction(action: string, showActionMgr: ShowActionManager): ActionParams<DefineActionParams<DefineActionGroupType>> {
+    var result:ActionParams<DefineActionParams<DefineActionGroupType>>={array:[]}
+    const arr = splitByWhiteSpace(action)
+ 
+    result.array = arr
+  
+    //convert to type
+    result.params = { type: arr[1],name:arr[2] }
+
+    const targets:string[] = []
+    for(let x=3;x<arr.length;x++){
+      const val:string[] = arr[x].split(",")
+      for(const p in val){
+        targets.push(val[p].trim())
+      }
+    }
+    result.params.opts = {actions:targets}
+    
+    return result 
+  }
+  process(action: ActionParams<DefineActionParams<DefineActionGroupType>>, showActionMgr: ShowActionManager): boolean {
+    //register the alias as a new action handler ... very meta
+ 
+    //TODO
+
+    return true
+  }
+}
+
 
 
 

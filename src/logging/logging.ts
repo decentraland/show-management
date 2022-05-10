f10bb92743d6cee2e0bde98f1a81289066506206
William Caine <william.caineiii@gmail.com>
2022-05-10 18:01:22 -0400

    breaking change. but new so ok? inverted logLevel enum values

--- src/logging/logging.ts
+++ src/logging/logging.ts
@@ -7,13 +7,15 @@
 /**
  * types of log levels
  */
+//export type LoggerLevel =  'ERROR' | 'WARN' | 'INFO'  | 'DEBUG' | 'TRACE' | 'LOG'
+
 export enum LogLevel{
-    LOG=0,
-    ERROR=1,
-    WARN=2,
-    INFO=3,
-    DEBUG=4,
-    TRACE=5
+    LOG=5,
+    ERROR=4,
+    WARN=3,
+    INFO=2,
+    DEBUG=1,
+    TRACE=0
 }
 
 /**
@@ -66,34 +68,41 @@ export class Logger {
     setLevel(level:LogLevel){
         this.level = level
     }
+
     //2021-03-16 13:58:10.817  INFO [traceid= spanid= parentspanid=] 58189 --- [           main] c.c.c.ConfigServicePropertySourceLocator : Fetching config from server at : http://localhost:8888
     trace(method:string,msg:string,...args:any){
-        this.logIt(LogLevel.TRACE,  method, msg, args);
+        if( LogLevel.TRACE < this.level ) return
+        this.logIt(LogLevel.TRACE,  method, msg, ...args);
     }
-    info(method:string,msg:string,...args:any){
-        this.logIt(LogLevel.INFO,  method, msg, args);
+    info(method:string,msg:string,...args:any[]){
+        if( LogLevel.INFO < this.level ) return
+        this.logIt(LogLevel.INFO,  method, msg, ...args);
     }
-    log(method:string,msg:string,...args:any){
-        this.logIt(LogLevel.LOG,  method, msg, args);
+    log(method:string,msg:string,...args:any[]){
+        if( LogLevel.LOG < this.level ) return
+        this.logIt(LogLevel.LOG,  method, msg, ...args);
     }
-    debug(method:string,msg:string,...args:any){
-        this.logIt(LogLevel.DEBUG,  method, msg, args);
+    debug(method:string,msg:string,...args:any[]){
+        if( LogLevel.DEBUG < this.level ) return
+        this.logIt(LogLevel.DEBUG,  method, msg, ...args);
     }
-    warn(method:string,msg:string,...args:any){
-        this.logIt(LogLevel.WARN,  method, msg, args);
+    warn(method:string,msg:string,...args:any[]){
+        if( LogLevel.WARN < this.level ) return
+        this.logIt(LogLevel.WARN,  method, msg, ...args);
     }
-    error(method:string,msg:string,...args:any){
-        this.logIt(LogLevel.ERROR,  method, msg, args);
+    error(method:string,msg:string,...args:any[]){
+        if( LogLevel.ERROR < this.level ) return
+        this.logIt(LogLevel.ERROR,  method, msg,...args);
     }
-    logIt(level:LogLevel,method:string,msg:string, ...args:any){
+    logIt(level:LogLevel,method:string,msg:string, ...args:any[]){
+        if(msg.indexOf("parse")){
+            //debugger
+        }
+        
         //log("TEST OG NOT ENABLED FOR  ",level,this.level, "level < this.level",level < this.level)
-        if( level > this.level ){
+        if( level < this.level ){
             //log("LOG.NOT ENABLED FOR  ",LogLevel[level],'default:',LogLevel[this.level])
             return 
-        }
-        let argsStr = null;
-        if(args){
-            argsStr=args
         }
         let contextStr = " "// + this.
         /*if(this.contextData){
@@ -103,18 +112,23 @@ export class Logger {
         }*/
         
         log(LogLevel[level] , contextStr , this.name , method ,":", msg , ...args)
+        
     }
 
     isWarnEnabled(){
-        return LogLevel.WARN <= this.level
+        return LogLevel.WARN >= this.level
     }
     isDebugEnabled(){
-        return LogLevel.DEBUG  <= this.level
+        return LogLevel.DEBUG  >= this.level
     }
     isInfoEnabled(){
-        return LogLevel.INFO  <= this.level
+        return LogLevel.INFO  >= this.level 
     }
     isTraceEnabled(){
-        return LogLevel.TRACE <= this.level
+        return LogLevel.TRACE >= this.level 
+    }
+    isErrorEnabled(){
+        return LogLevel.ERROR >= this.level
     }
 }
+
